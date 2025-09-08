506c5ef4f271f6b2f0603688947fd0889576e456
Faraz Husain <fhusain@wr.iitr.ac.in>
2025-09-08 13:08:32 +0530

    delete transaction API added

--- backend/server.js
+++ backend/server.js
@@ -69,6 +69,30 @@ app.post("/api/transactions", async (req, res) => {
   }
 });
 
+app.delete("/api/transactions/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (isNaN(parseInt(id))) {
+      return res.status(400).json({ message: "Invalid id" });
+    }
+
+    const results = await sql`
+        DELETE FROM transactions WHERE id = ${id}
+        RETURNING *
+    `;
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: "Transaction not found" });
+    }
+
+    res.status(200).json({ message: "Transaction deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting the transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 initDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is up and running on PORT: ${PORT}`);
