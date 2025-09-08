248c0bc6f2a3e83f9478b3e1c5907519d594ebdd
Faraz Husain <fhusain@wr.iitr.ac.in>
2025-09-08 21:25:52 +0530

    DB code refactoring

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { sql } from "./config/db.js";
+import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoutes from "./routes/transactionsRoutes.js";
 
@@ -14,26 +14,8 @@ app.use(express.json());
 
 const PORT = process.env.PORT;
 
-async function initDB(params) {
-  try {
-    await sql`CREATE TABLE IF NOT EXISTS transactions(
-        id SERIAL PRIMARY KEY,
-        user_id VARCHAR(255) NOT NULL,
-        title VARCHAR(255) NOT NULL,
-        amount DECIMAL(10,2) NOT NULL,
-        category VARCHAR(255) NOT NULL,
-        created_at DATE NOT NULL DEFAULT CURRENT_DATE
-    )`;
-
-    console.log("Database initialized successfully");
-  } catch (error) {
-    console.log("Error initializing DB: ", error);
-    process.exit(1); // Status code 1 means failure, 0 is success
-  }
-}
-
-app.get("/", (req, res) => {
-  res.send("It's working...");
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
 });
 
 app.use("/api/transactions", transactionsRoutes);
