1a575086a31e2dc49a695f8d5f3f1a2bfaf7dd29
Faraz Husain <fhusain@wr.iitr.ac.in>
2025-09-09 17:49:39 +0530

    Added bottom padding to SafeScreen component

--- frontend/components/SafeScreen.jsx
+++ frontend/components/SafeScreen.jsx
@@ -9,6 +9,7 @@ const SafeScreen = ({ children }) => {
     <View
       style={{
         paddingTop: insets.top,
+        paddingBottom: insets.bottom,
         flex: 1,
         backgroundColor: COLORS.background,
       }}
